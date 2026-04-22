c9ebf4c8b0f53ed435640da670b578214a7ae256
toasterbath0758 <toasterbath0758@gmail.com>
2026-04-22 13:50:25 +0200

    styling van de POST knop (success-state) #9

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 // addEventListener
 // verander iets
 
+const success = document.querySelectorAll('.button.verstuur');
+
+success.addEventListener('click', function(){
+  success.classList.toggle('active');
+})
+
